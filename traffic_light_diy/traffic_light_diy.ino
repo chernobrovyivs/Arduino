152d8983e3bc7c397e89f29f95e0bac9b87d887e
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-01-20 21:37:50 +0300

    Update traffic_light sketch
    Update traffic_light sketch.
    Version 0.3.

--- traffic_light_diy/traffic_light_diy.ino
+++ traffic_light_diy/traffic_light_diy.ino
@@ -1,6 +1,6 @@
 /*
   Traffic light sketch for Arduino UNO R3
-  version: 0.2
+  version: 0.3
   date: 20.01.2023
   Developer: Valeriy Chernobrovyi (chernobrovyivs)
 */
@@ -8,50 +8,45 @@ byte LEDcounter = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(1, OUTPUT); // Red light
-  pinMode(2, OUTPUT); // Yellow light
-  pinMode(3, OUTPUT); // Green light
+  pinMode(2, OUTPUT); // Red light
+  pinMode(3, OUTPUT); // Yellow light
+  pinMode(4, OUTPUT); // Green light
 
-  digitalWrite(1, 0);
   digitalWrite(2, 0);
   digitalWrite(3, 0);
+  digitalWrite(4, 0);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while(LEDcounter <= 3){
+  LEDcounter = 2;
+  while(LEDcounter <= 4){
     switch(LEDcounter){
-      case 0:
-        digitalWrite(1, 0);
-        digitalWrite(2, 0);
+      case 2:
+        digitalWrite(2, 1);
         digitalWrite(3, 0);
-        break;
-      case 1:
-        digitalWrite(1, 1);
-        digitalWrite(2, 0);
-        digitalWrite(3, 0);
+        digitalWrite(4, 0);
 
         Serial.println("Red light.");
         break;
-      case 2:
-        digitalWrite(1, 0);
-        digitalWrite(2, 1);
-        digitalWrite(3, 0);
+      case 3:
+        digitalWrite(2, 0);
+        digitalWrite(3, 1);
+        digitalWrite(4, 0);
 
         Serial.println("Yellow light.");
         break;
-      case 3:
-        digitalWrite(1, 0);
+      case 4:
         digitalWrite(2, 0);
-        digitalWrite(3, 1);
+        digitalWrite(3, 0);
+        digitalWrite(4, 1);
 
         Serial.println("Green light.");
         break;
       default:
-        digitalWrite(1, 0);
         digitalWrite(2, 0);
+        digitalWrite(3, 0);
         digitalWrite(3, 0);
         break;
     }
@@ -60,7 +55,7 @@ void loop() {
     delay(2000);
   }
 
-  if(LEDcounter > 3){
+  if(LEDcounter > 4){
     LEDcounter = 0;
     Serial.println("Переменная LEDcounter == 0!");
     delay(2000);
