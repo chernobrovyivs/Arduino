6762bd048290dfb2fa3af2d33fb194a8ff1832f3
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-01-20 21:00:12 +0300

    Add new sketch Traffic lights
    Add new sketch Traffic lights!
    Version 0.1

---
+++ traffic_light_diy/traffic_light_diy.ino
@@ -0,0 +1,57 @@
+/*
+  Traffic light sketch for Arduino UNO R3
+  version: 0.1
+  Developer: Valeriy Chernobrovyi (chernobrovyivs)
+*/
+byte LEDcounter = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(1, OUTPUT); // Red light
+  pinMode(2, OUTPUT); // Yellow light
+  pinMode(3, OUTPUT); // Green light
+
+  digitalWrite(1, 0);
+  digitalWrite(2, 0);
+  digitalWrite(3, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(LEDcounter <= 3){
+    switch(LEDcounter){
+      case 0:
+        digitalWrite(1, 0);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        break;
+      case 1:
+        digitalWrite(1, 1);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        break;
+      case 2:
+        digitalWrite(1, 0);
+        digitalWrite(2, 1);
+        digitalWrite(3, 0);
+        break;
+      case 3:
+        digitalWrite(1, 0);
+        digitalWrite(2, 0);
+        digitalWrite(3, 1);
+        break;
+      default:
+        digitalWrite(1, 0);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        break;
+    }
+
+    LEDcounter++;
+    delay(2000);
+  }
+
+  if(LEDcounter > 3){
+    LEDcounter = 0;
+  }
+}
