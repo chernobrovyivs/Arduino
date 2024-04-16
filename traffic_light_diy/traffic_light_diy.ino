f6cef4ced1ab8973d4db5bd52cad2d7812a88c56
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-04-16 22:11:31 +0300

    Edit traffic_light_diy ver and func
    Project "traffic_light_diy" add ver. 05.
    Modified 16.04.2024

--- traffic_light_diy/traffic_light_diy.ino
+++ traffic_light_diy/traffic_light_diy.ino
@@ -1,16 +1,22 @@
 /*
   Traffic light sketch for Arduino UNO R3
-  version: 0.3
+  version: 0.5
   date: 20.01.2023
+  modified 16.04.2023
   Developer: Valeriy Chernobrovyi (chernobrovyivs)
 */
 byte LEDcounter = 0;
+boolean btn_flag = 0;
+boolean btn_status = 0;
+boolean btn_status_work_const = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(2, OUTPUT); // Red light
   pinMode(3, OUTPUT); // Yellow light
   pinMode(4, OUTPUT); // Green light
+
+  pinMode(8, INPUT_PULLUP); // btn initial
 
   digitalWrite(2, 0);
   digitalWrite(3, 0);
@@ -20,44 +26,69 @@ void setup() {
 }
 
 void loop() {
-  LEDcounter = 2;
-  while(LEDcounter <= 4){
-    switch(LEDcounter){
-      case 2:
-        digitalWrite(2, 1);
+  Serial.println("Выберите режим работы!");
+  delay(2000);
+  btn_status = !digitalRead(8); // Read btn status!
+  btn_status_work_const = 0;
+  Serial.println(btn_status);
+
+  if(btn_status == 1 && btn_flag == 0){
+    btn_status_work_const = 1;
+    btn_flag = 1;
+    delay(2000);
+  } else if(btn_status == 0 && btn_flag == 1){
+    btn_status_work_const = 0;
+    btn_flag = 0;
+    delay(2000);
+  }
+
+    if((btn_status_work_const == 1) && (btn_flag == 1)){
+      Serial.println("Включен автоматический режим!\n");
+      LEDcounter = 2;
+      while(LEDcounter <= 4){
+        switch(LEDcounter){
+          case 2:
+            digitalWrite(2, 1);
+            digitalWrite(3, 0);
+            digitalWrite(4, 0);
+
+            Serial.println("Red light.\n");
+            break;
+          case 3:
+            digitalWrite(2, 0);
+            digitalWrite(3, 1);
+            digitalWrite(4, 0);
+
+            Serial.println("Yellow light.\n");
+            break;
+          case 4:
+            digitalWrite(2, 0);
+            digitalWrite(3, 0);
+            digitalWrite(4, 1);
+
+            Serial.println("Green light.\n");
+            break;
+          default:
+            digitalWrite(2, 0);
+            digitalWrite(3, 0);
+            digitalWrite(4, 0);
+            break;
+          }
+
+        LEDcounter++;
+        delay(2000);
+        digitalWrite(2, 0);
         digitalWrite(3, 0);
         digitalWrite(4, 0);
+      }
 
-        Serial.println("Red light.");
-        break;
-      case 3:
-        digitalWrite(2, 0);
-        digitalWrite(3, 1);
-        digitalWrite(4, 0);
-
-        Serial.println("Yellow light.");
-        break;
-      case 4:
-        digitalWrite(2, 0);
-        digitalWrite(3, 0);
-        digitalWrite(4, 1);
-
-        Serial.println("Green light.");
-        break;
-      default:
-        digitalWrite(2, 0);
-        digitalWrite(3, 0);
-        digitalWrite(3, 0);
-        break;
+      if(LEDcounter > 4){
+        LEDcounter = 0;
+        Serial.println("Переменная LEDcounter == 0!\n");
+        delay(2000);
+      }
+    } else{
+      Serial.println("Выключен автоматический режим!\n");
+      delay(2000);
     }
-
-    LEDcounter++;
-    delay(2000);
-  }
-
-  if(LEDcounter > 4){
-    LEDcounter = 0;
-    Serial.println("Переменная LEDcounter == 0!");
-    delay(2000);
-  }
 }
