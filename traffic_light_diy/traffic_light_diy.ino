7c4affa7c8acea51464edbe82a04322b19570927
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-04-21 12:53:20 +0300

    Fix error in vers 0.5.1 -> 0.5.2
    Fix error in vers 0.5.1 -> 0.5.2. End new structure.

--- traffic_light_diy/traffic_light_diy.ino
+++ traffic_light_diy/traffic_light_diy.ino
@@ -1,10 +1,12 @@
 /*
   Traffic light sketch for Arduino UNO R3
-  version: 0.5.1
+  version: 0.5.2
   date: 20.01.2023
   modified 20.04.2023
   Developer: Valeriy Chernobrovyi (chernobrovyivs)
 */
+
+String version_po = "0.5.2";
 byte LEDcounter = 0;
 boolean btn_flag = 0;
 boolean btn_status = 0;
@@ -25,38 +27,7 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  Serial.println("Выберите режим работы!");
-  delay(2000);
-  btn_status = !digitalRead(8); // Read btn status!
-  //btn_status_work_const = 0;
-  Serial.println(btn_status);
-
-  if(btn_status == 1 && btn_flag == 0){
-    btn_status_work_const = 1;
-    btn_flag = 1;
-    delay(2000);
-  } else if(btn_status == 0 && btn_flag == 1){
-    btn_status_work_const = 0;
-    btn_flag = 0;
-    delay(2000);
-  }
-
-    if((btn_status_work_const == 1) && (btn_flag == 1)){
-      automode();
-      }
-
-      if(LEDcounter > 4){
-        LEDcounter = 0;
-        Serial.println("Переменная LEDcounter == 0!\n");
-        delay(2000);
-      }
-    } else{
-      manualmode();
-    }
-}
-
-void automode(){
+void automode_fn(){
   Serial.println("Включен автоматический режим!\n");
       LEDcounter = 2;
       while(LEDcounter <= 4){
@@ -87,16 +58,50 @@ void automode(){
             digitalWrite(3, 0);
             digitalWrite(4, 0);
             break;
-          }
+        }
 
         LEDcounter++;
         delay(2000);
         digitalWrite(2, 0);
         digitalWrite(3, 0);
         digitalWrite(4, 0);
+      }
 }
 
-void manualmode(){
+void manualmode_fn(){
   Serial.println("Включён ручной режим!\n");
   delay(2000);
+}
+
+void loop(){
+  Serial.println("Версия ПО: " + version_po);
+  delay(1000);
+  Serial.println("Выберите режим работы!");
+  delay(2000);
+  btn_status = !digitalRead(8); // Read btn status!
+  //btn_status_work_const = 0;
+  Serial.println(btn_status);
+
+  if(btn_status == 1 && btn_flag == 0){
+    btn_status_work_const = 1;
+    btn_flag = 1;
+    delay(2000);
+  } else if(btn_status == 0 && btn_flag == 1){
+    btn_status_work_const = 0;
+    btn_flag = 0;
+    delay(2000);
+  }
+
+  if((btn_status_work_const == 1) && (btn_flag == 1)){
+    automode_fn();
+
+    if(LEDcounter > 4){
+      LEDcounter = 0;
+      Serial.println("Переменная LEDcounter == 0!\n");
+      delay(2000);
+    }
+  }
+  else {
+    manualmode_fn();
+  }
 }
